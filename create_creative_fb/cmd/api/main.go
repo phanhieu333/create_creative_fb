e6f95cfc781c492648ae3a4b718c39c581ceb1b4
PTHieu <hieupt@interdogmedia.com>
2026-01-21 17:33:06 +0700

    duma

--- create_creative_fb/cmd/api/main.go
+++ create_creative_fb/cmd/api/main.go
@@ -14,26 +14,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type CreativeInput struct {
-	Type                  string
-	AccountID             string
-	PageID                string
-	Name                  string
-	ImageHash             string
-	Link                  string
-	Picture               string
-	Message               string
-	VideoID               string
-	Thumbnail             string
-	PictureURL            string
-	ChildAttachmentsInput []string
-}
-
 func main() {
 	_ = godotenv.Load()
 
 	input := parseFlags()
-	validateInput(input)
 
 	accessToken := os.Getenv("FACEBOOK_ACCESS_TOKEN")
 	client := facebook.NewClient(accessToken)
@@ -53,8 +37,8 @@ func (i *arrayFlags) Set(value string) error {
 	*i = append(*i, value)
 	return nil
 }
-func parseFlags() CreativeInput {
-	input := CreativeInput{}
+func parseFlags() facebook.CreativeInput {
+	input := facebook.CreativeInput{}
 
 	var childAttachments arrayFlags
 
@@ -68,45 +52,21 @@ func parseFlags() CreativeInput {
 	flag.StringVar(&input.Message, "message", "", "Creative message")
 	flag.StringVar(&input.VideoID, "video", "", "Video ID")
 	flag.StringVar(&input.Thumbnail, "thumbnail", "", "Thumbnail URL")
-	flag.StringVar(&input.Picture, "picture", "", "Picture URL")
 	flag.Var(
 		&childAttachments,
 		"child_attachment",
 		"Carousel child attachment: name=...,image_hash=...,desc=...,link=...,video_id=...",
 	)
 	flag.Parse()
-	input.ChildAttachmentsInput = childAttachments
+	parsedChildren, err := parseChildAttachments(childAttachments)
+	if err != nil {
+		log.Fatal(err)
+	}
+	input.ChildAttachmentsInput = parsedChildren
 	return input
 }
-func validateInput(in CreativeInput) {
-	if in.Type == "" {
-		log.Fatal("Missing -type")
-	}
 
-	if in.AccountID == "" || in.PageID == "" {
-		log.Fatal("Missing -account or -page")
-	}
-
-	if in.Type == "single_video" {
-		if in.VideoID == "" {
-			log.Fatal("Missing -video Id")
-		}
-		if in.Thumbnail == "" {
-			log.Fatal("Missing -thumbnail")
-		}
-	}
-
-	if in.Type == "carousel" {
-		if len(in.ChildAttachmentsInput) < 2 {
-			log.Fatal("At least two -child_attachment are required for carousel")
-		}
-		if in.ImageHash == "" && in.Picture == "" {
-			log.Fatal("Missing -image or -picture for carousel")
-		}
-	}
-
-}
-func dispatchCreate(client *facebook.Client, in CreativeInput) error {
+func dispatchCreate(client *facebook.Client, in facebook.CreativeInput) error {
 	switch in.Type {
 	case "single_image":
 		return createSingleImage(client, in)
@@ -118,7 +78,9 @@ func dispatchCreate(client *facebook.Client, in CreativeInput) error {
 		return fmt.Errorf("unsupported creative type: %s", in.Type)
 	}
 }
-func createSingleImage(client *facebook.Client, in CreativeInput) error {
+func createSingleImage(client *facebook.Client, in facebook.CreativeInput) error {
+	facebook.ValidateInput(in)
+
 	creative := dto.CreateCreativeSingleImageRequest{
 		ObjectStorySpecImage: model.ImageDetail{
 			PageID: in.PageID,
@@ -138,7 +100,9 @@ func createSingleImage(client *facebook.Client, in CreativeInput) error {
 	return nil
 }
 
-func createSingleVideo(client *facebook.Client, in CreativeInput) error {
+func createSingleVideo(client *facebook.Client, in facebook.CreativeInput) error {
+	facebook.ValidateInput(in)
+
 	creative := dto.CreateCreativeSingleVideoRequest{
 		ObjectStorySpecVideo: model.VideoDetail{
 			PageID: in.PageID,
@@ -159,14 +123,15 @@ func createSingleVideo(client *facebook.Client, in CreativeInput) error {
 	return nil
 }
 
-func createCreativeCarousel(client *facebook.Client, in CreativeInput) error {
-	childAttachments, err := parseChildAttachmentsInput(in.ChildAttachmentsInput)
+func createCreativeCarousel(client *facebook.Client, in facebook.CreativeInput) error {
+	facebook.ValidateInput(in)
+
 	creative := dto.CreateCreativeCarouselRequest{
 		ObjectStorySpecCarousel: model.CarouselDetail{
 			PageID: in.PageID,
 			LinkData: model.LinkDataCarousel{
 				Link:             in.Link,
-				ChildAttachments: childAttachments,
+				ChildAttachments: in.ChildAttachmentsInput,
 			},
 		},
 	}
@@ -181,31 +146,39 @@ func createCreativeCarousel(client *facebook.Client, in CreativeInput) error {
 	fmt.Printf("  Creative ID: %s\n", res.ID)
 	return nil
 }
-func parseChildAttachmentsInput(
-	input []string,
-) ([]model.ChildAttachmentsData, error) {
 
-	var attachments []model.ChildAttachmentsData
+func parseChildAttachments(inputs []string) ([]model.ChildAttachmentsData, error) {
+	var result []model.ChildAttachmentsData
 
-	for _, raw := range input {
-		fields := map[string]string{}
+	for _, raw := range inputs {
+		item := model.ChildAttachmentsData{}
 
-		for _, part := range strings.Split(raw, ",") {
+		parts := strings.Split(raw, ",")
+		for _, part := range parts {
 			kv := strings.SplitN(part, "=", 2)
 			if len(kv) != 2 {
 				return nil, fmt.Errorf("invalid child_attachment format: %s", raw)
 			}
-			fields[kv[0]] = kv[1]
+
+			key := kv[0]
+			val := kv[1]
+
+			switch key {
+			case "name":
+				item.Name = val
+			case "image_hash":
+				item.ImageHash = val
+			case "link":
+				item.Link = val
+			case "desc":
+				item.Description = val
+			case "video_id":
+				item.VideoID = val
+			}
 		}
 
-		attachments = append(attachments, model.ChildAttachmentsData{
-			Name:        fields["name"],
-			Description: fields["desc"],
-			ImageHash:   fields["image_hash"],
-			Link:        fields["link"],
-			VideoID:     fields["video_id"],
-		})
+		result = append(result, item)
 	}
 
-	return attachments, nil
+	return result, nil
 }
